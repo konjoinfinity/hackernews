cf05c19cc4a12aa087a8fb5db4bd8d908f3aab4f
Wesley Scholl <clients@konjoweb.com>
2019-02-22 16:00:00 -0500

    added script and code for deployment

--- db/connection.js
+++ db/connection.js
@@ -3,3 +3,9 @@ mongoose.connect("mongodb://localhost/hackernews");
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
+
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/hackernews");
+}
--- index.js
+++ index.js
@@ -10,3 +10,9 @@ app.use(methodOverride("_method"));
 app.use(require("./routes/index"));
 
 app.listen(3000, () => console.log("listening on port 3000"));
+
+app.set("port", process.env.PORT || 3001);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
