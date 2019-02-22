70e091a8e4c4b93509ac7f5b78192d41133bc155
Wesley Scholl <clients@konjoweb.com>
2019-02-22 16:53:51 -0500

    url parser update

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/hackernews");
+mongoose.connect("mongodb://localhost/hackernews"), { useNewUrlParser: true };
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
@@ -7,5 +7,5 @@ module.exports = mongoose;
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL);
 } else {
-  mongoose.connect("mongodb://localhost/hackernews");
+  mongoose.connect("mongodb://localhost/hackernews"), { useNewUrlParser: true };
 }
