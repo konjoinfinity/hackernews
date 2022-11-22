644d4af8d5ad47afb24f539e072e2cdd453f987a
Wesley Scholl <clients@konjoweb.com>
2022-11-22 17:09:52 -0500

    updated all node deps

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,27 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL), { useNewUrlParser: true };
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    family: 4
+  }, function(err) {
+    if (err) {throw err;
+    } else {console.log("Production Database Connection Successful");}
+    })
 } else {
-  mongoose.connect("mongodb://localhost/hackernews"), { useNewUrlParser: true };
+  mongoose.connect("mongodb://127.0.0.1/hackernews"), { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    family: 4
+  }, function(err) {
+    if (err) {throw err;
+    } else {console.log("Development Database Connection Successful")}
+    };
 }
 
 module.exports = mongoose;
