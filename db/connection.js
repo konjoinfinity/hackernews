929e7413ae009d480068fbce8eca7bc6d89b0b60
Wesley Scholl <clients@konjoweb.com>
2022-11-22 18:10:54 -0500

    updated mongoose connection

--- db/connection.js
+++ db/connection.js
@@ -3,22 +3,12 @@ mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    family: 4
   }, function(err) {
     if (err) {throw err;
     } else {console.log("Production Database Connection Successful");}
     })
 } else {
   mongoose.connect("mongodb://127.0.0.1/hackernews"), { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    family: 4
   }, function(err) {
     if (err) {throw err;
     } else {console.log("Development Database Connection Successful")}
